fb71581be391f62feb2aee5812b3890d00c15a1c
Maz Radwan <maz.radwan@keyin.com>
2024-10-17 11:01:02 -0230

    update

--- sportsService.js
+++ sportsService.js
@@ -1,14 +1,29 @@
-const getNews = async () => {
-  const apiKey = process.env.NEWS_API_KEY;
-  const response = await fetch(
-    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}&pageSize=50`
-  );
-  if (!response.ok) {
-    console.error(`Error fetching news data: HTTP Status: ${response.status}`);
-    const errorText = await response.text(); // Log response body in case it's useful
-    console.error("Response body:", errorText);
-    throw new Error(`HTTP error! Status: ${response.status}`);
-  }
-  const data = await response.json();
-  return data.articles;
+const https = require("https");
+
+const fetchMLBData = (date, callback) => {
+  const API_KEY = process.env.SPORTS_API_KEY;
+  const url = `https://api.sportsdata.io/v3/mlb/scores/json/GamesByDate/${date}?key=${API_KEY}`;
+
+  const req = https.get(url, (resp) => {
+    let data = "";
+    resp.on("data", (chunk) => {
+      data += chunk;
+    });
+    resp.on("end", () => {
+      callback(JSON.parse(data));
+    });
+  });
+
+  req.on("error", (err) => {
+    console.error("Error fetching MLB data:", err.message);
+  });
+
+  req.setTimeout(5000, () => {
+    req.abort();
+    console.error("Request timed out");
+  });
+};
+
+module.exports = {
+  fetchMLBData,
 };
